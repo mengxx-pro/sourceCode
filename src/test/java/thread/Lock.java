4ff77254e910033396109d495b5d5bb5f48e7082
mengxiangxing <1071551282@qq.com>
2021-09-17 19:13:33 +0800

    aqs

---
+++ src/test/java/thread/Lock.java
@@ -0,0 +1,13 @@
+package thread;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author: mengxiangxing
+ * @description: aqs cas等锁
+ * @create: 2021-09-17 17:05
+ **/
+public class Lock {
+
+  AbstractQueuedSynchronizer
+}
