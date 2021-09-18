528135f6534d902e650192f6ab4372ca2201581e
mengxiangxing <1071551282@qq.com>
2021-09-18 18:10:14 +0800

    reentrantLock

--- src/test/java/thread/Lock.java
+++ src/test/java/thread/Lock.java
@@ -1,6 +1,7 @@
 package thread;
 
-import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author: mengxiangxing
@@ -9,5 +10,49 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
  **/
 public class Lock {
 
-  AbstractQueuedSynchronizer
+  /*AbstractQueuedSynchronizer;*/
+
+  private static final ReentrantLock reentrantLock = new ReentrantLock();
+
+
+  public static void main(String[] args) {
+
+
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        reentrantLock.lock();
+        System.out.println("加锁1"+new Date());
+        try {
+          System.out.println("try1"+new Date());
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          reentrantLock.unlock();
+          System.out.println("解锁1"+new Date());
+        }
+      }
+    }).start();
+
+
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        reentrantLock.lock();
+        System.out.println("加锁2"+new Date());
+        try {
+          System.out.println("try2"+new Date());
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          reentrantLock.unlock();
+          System.out.println("解锁2"+new Date());
+        }
+      }
+    }).start();
+
+
+  }
 }
