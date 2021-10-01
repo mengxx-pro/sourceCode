8c2e841c1ccbb5a72171cf47f91ec60fc91498fb
mengxiangxing <1071551282@qq.com>
2021-10-01 17:36:09 +0800

    java8

---
+++ src/test/java/Java8.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+/**
+ * @Author: mengxiangxing
+ * @Description: java8 新特性
+ * @Date: 2021/9/30 15:23
+ */
+public class Java8 {
+
+    private static List<StudentScore> studentScoreList = new ArrayList<>();
+
+    public  Java8() {
+
+        StudentScore studentScore1 = new StudentScore() {{
+            setStuName("张三");
+            setSubject("语文");
+            setScore(70);
+        }};
+        StudentScore studentScore2 = new StudentScore() {{
+            setStuName("张三");
+            setSubject("数学");
+            setScore(80);
+        }};
+        StudentScore studentScore3 = new StudentScore() {{
+            setStuName("张三");
+            setSubject("英语");
+            setScore(65);
+        }};
+        StudentScore studentScore4 = new StudentScore() {{
+            setStuName("李四");
+            setSubject("语文");
+            setScore(68);
+        }};
+        StudentScore studentScore5 = new StudentScore() {{
+            setStuName("李四");
+            setSubject("数学");
+            setScore(70);
+        }};
+        StudentScore studentScore6 = new StudentScore() {{
+            setStuName("李四");
+            setSubject("英语");
+            setScore(90);
+        }};
+        StudentScore studentScore7 = new StudentScore() {{
+            setStuName("王五");
+            setSubject("语文");
+            setScore(80);
+        }};
+        StudentScore studentScore8 = new StudentScore() {{
+            setStuName("王五");
+            setSubject("数学");
+            setScore(85);
+        }};
+        StudentScore studentScore9 = new StudentScore() {{
+            setStuName("王五");
+            setSubject("英语");
+            setScore(70);
+        }};
+
+        studentScoreList.add(studentScore1);
+        studentScoreList.add(studentScore2);
+        studentScoreList.add(studentScore3);
+        studentScoreList.add(studentScore4);
+        studentScoreList.add(studentScore5);
+        studentScoreList.add(studentScore6);
+        studentScoreList.add(studentScore7);
+        studentScoreList.add(studentScore8);
+        studentScoreList.add(studentScore9);
+    }
+
+    public static void main(String[] args) {
+        //1排序
+        List<String> name = Arrays.asList("tom", "bob", "six", "exe");
+        //常规排序
+        /*Collections.sort(name, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });*/
+        //java8排序
+        //Collections.sort(name);
+        /*Collections.sort(name,(a,b)->{
+            return a.compareTo(b);
+        });*/
+        Collections.sort(name, (a, b) -> a.compareTo(b));
+        System.out.println(name);
+
+        new Java8();
+        Map<String, Integer> studentScoreMap2 = new HashMap<>();
+        studentScoreList.forEach(studentScore -> studentScoreMap2.merge(
+                studentScore.getStuName(),
+                studentScore.getScore(),
+                Integer::sum));
+
+
+
+        System.out.println(studentScoreMap2);
+    }
+
+
+}
---
+++ src/test/java/StudentScore.java
@@ -0,0 +1,35 @@
+/**
+ * @Author: mengxiangxing
+ * @Description: TODO
+ * @Date: 2021/10/1 17:16
+ */
+public class StudentScore {
+
+    private String stuName;
+    private String subject;
+    private Integer score;
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
