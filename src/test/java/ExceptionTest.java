f6101a4c500383824ad14f909c8952a8126abb3a
mengxiangxing <1071551282@qq.com>
2021-10-09 19:43:34 +0800

    fix

--- src/test/java/ExceptionTest.java
+++ src/test/java/ExceptionTest.java
@@ -7,34 +7,34 @@ import java.util.Arrays;
  **/
 public class ExceptionTest implements AutoCloseable {
 
-  public static int returnInt(){
-    int a=1;
-    try{
-      return a;
-    }finally {
-      a=3;
-      System.out.println("returnInt里的值是"+a);
+    public static int returnInt() {
+        int a = 1;
+        try {
+            return a;
+        } finally {
+            a = 3;
+            System.out.println("returnInt里的值是" + a);
+        }
     }
-  }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String a="123552232";
-    int x= a.charAt(0)-'0';
-    System.out.println(x);
-    /*当try和finally中也有return时，会最后执行finally里的并覆盖try中的返回值
-     *finally 块： 无论是否捕获或处理异常，finally 块里的语句都会被执行。
-     *  当在 try 块或 catch 块中遇到 return 语句时，finally 语句块将在方法返回之前被执行。
-     */
+        String a = "123552232";
+        int x = a.charAt(0) - '0';
+        System.out.println(x);
+        /*当try和finally中也有return时，会最后执行finally里的并覆盖try中的返回值
+         *finally 块： 无论是否捕获或处理异常，finally 块里的语句都会被执行。
+         *  当在 try 块或 catch 块中遇到 return 语句时，finally 语句块将在方法返回之前被执行。
+         */
 
-    //System.out.println(ExceptionTest.returnInt());
+        //System.out.println(ExceptionTest.returnInt());
 
 
-    /**
-     * 需要关闭资源的不要手动close直接用try-with-resources来自动关闭
-     * 适用范围（资源的定义）： 任何实现 java.lang.AutoCloseable或者 java.io.Closeable 的对象
-     * 关闭资源和 finally 块的执行顺序： 在 try-with-resources 语句中，任何 catch 或 finally 块在声明的资源关闭后运行
-     */
+        /**
+         * 需要关闭资源的不要手动close直接用try-with-resources来自动关闭
+         * 适用范围（资源的定义）： 任何实现 java.lang.AutoCloseable或者 java.io.Closeable 的对象
+         * 关闭资源和 finally 块的执行顺序： 在 try-with-resources 语句中，任何 catch 或 finally 块在声明的资源关闭后运行
+         */
    /* ExceptionTest exceptionTest =new ExceptionTest();
     try {
       exceptionTest.doIt();
@@ -49,14 +49,14 @@ public class ExceptionTest implements AutoCloseable {
     }catch (Exception e){
       e.printStackTrace();
     }*/
-  }
+    }
 
-  @Override
-  public void close() {
-    System.out.println("closed.....");
-  }
+    @Override
+    public void close() {
+        System.out.println("closed.....");
+    }
 
-  public static void doIt() throws Exception {
-    System.out.println("doIt.....");
-  }
+    public static void doIt() throws Exception {
+        System.out.println("doIt.....");
+    }
 }
