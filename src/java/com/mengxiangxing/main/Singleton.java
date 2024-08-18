e93a64267d52eebc91117fcafc52c63435c7a52b
mengxiangxing <1071551282@qq.com>
2024-08-18 16:03:17 +0800

    refactor(spring-boot): 升级到2.0.7.RELEASE版本并调整依赖
    -将Spring Boot Starter Parent升级到2.0.7.RELEASE版本- 调整了部分依赖项，包括Guava和Lombok的版本
    
    fix(JavaTest): 确保在比较字符串时正确使用intern方法
    
    - 修复了JavaTest类中的字符串比较逻辑，以在比较字符串时使用intern方法
    - 这确保了字符串比较基于它们在常量池中的引用，而不是它们的equals方法feat(MainApplication): 添加Singleton实例化演示
    
    - 在MainApplication类中添加了Singleton类的实例化演示
    - 此更改展示了如何通过Singleton模式创建对象实例
    
    refactor(LambdaTest): 重构generateAll方法并添加新测试
    
    -重构了LambdaTest类中的generateAll方法，以有效生成所有可能的组合
    - 添加了新测试以验证reduce和orElse方法的行为
    
    BREAKING CHANGE: 调整了某些依赖项的版本，这可能会影响依赖这些库的现有客户端。请检查更新后的依赖项版本以确保兼容性。

--- src/java/com/mengxiangxing/main/Singleton.java
+++ src/java/com/mengxiangxing/main/Singleton.java
@@ -1,5 +1,7 @@
 package com.mengxiangxing.main;
 
+import java.util.Arrays;
+
 /**
  * 单例模式
  */
@@ -7,15 +9,15 @@ public class Singleton {
     //懒汉模式
     private static Singleton singleton;
 
-    public static Singleton getInstance(){
-        if(singleton==null){
-            synchronized(Singleton.class){
-                if(singleton==null){
-                    singleton =new Singleton();
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
                 }
             }
         }
-         return singleton;
+        return singleton;
     }
 
     public Singleton() {
@@ -34,14 +36,45 @@ public class Singleton {
     }*/
 
     public static void main(String[] args) {
-        int array[] = new int[]{10, 2, -11, 1, 5};
-        //连续的子数组，相加之和的最大值
-        int max = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-               /* i
-                j*/
-        }
+        int[] array = new int[]{13, -2, -11, -1, 5, 2, 5};
+        //连续的子数组，求各个子数组相加之和后的最大值
 
+        /**
+         * 1.双循环来获取,暴力法
+         */
+      /*  int max = array[0] + array[1];
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    System.out.print(k + " ");
+                    sum = sum + array[k];
+                }
+                System.out.println();
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        System.out.println("最大值为：" + max);*/
+
+
+        /**
+         * 2. 利用当前最大值和全局最大值的比较来区分
+         *  2.1 如果前边的子数组加上当前的数大于当前的数则认为应该加上
+         *  2.2 否则就重新记录当前最大值为当前数字
+         */
+        //当前最大值
+        int currentMax = array[0] +array[1];
+        //全局最大值
+        int globalMax = array[0]+array[1];
+        for (int i = 2; i < array.length; i++) {
+            currentMax =Math.max(currentMax+array[i],array[i]);
+            System.out.println("当前最大值为：" + currentMax);
+            globalMax =Math.max(currentMax,globalMax);
+            System.out.println("当前全局最大值为：" + globalMax);
+        }
+        System.out.println("====最大值为：" + globalMax);
     }
 
 
